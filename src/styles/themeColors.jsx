cfbf79842c928a0babcf7a76710b0c567018c8d4
pricilliaedou <pricillia.edou@gmail.com>
2025-09-28 22:37:55 +0200

    themecolors

---
+++ src/styles/themeColors.jsx
@@ -0,0 +1,22 @@
+export const themeColors = {
+  violet: {
+    light: "#C9C8FF",
+    main: "#A09EFD",
+    dark: "#6B69E6",
+  },
+  yellow: {
+    light: "#FFF1C9",
+    main: "#FFD45C",
+    dark: "#FFC133",
+  },
+  teal: {
+    light: "#E2F2E2",
+    main: "#B6F2E2",
+    dark: "#99E6E6",
+  },
+  orange: {
+    light: "#FFE2E2",
+    main: "#FF9979",
+    dark: "#FF795C",
+  },
+};
