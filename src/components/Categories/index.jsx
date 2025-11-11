9d982f7fe68eb77c1de16cc83a667e9ed0959fa4
pricilliaedou <pricillia.edou@gmail.com>
2025-11-11 09:06:08 +0100

    When clicking on a category, redirection to video after user authentication

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import {
   Sommeil,
   Cerveau,
@@ -15,36 +16,36 @@ const Categories = ({ isAuthenticated = false }) => {
 
   return (
     <div className="categories">
-      <a href={linkPath} className="categories-cerveau">
+      <Link to={linkPath} className="categories-cerveau">
         <Cerveau className="inner" />
         <span className="categories-text">Cerveau</span>
-      </a>
-      <a href={linkPath} className="categories-cyberharcelement">
+      </Link>
+      <Link to={linkPath} className="categories-cyberharcelement">
         <Cyberharcelement className="inner" />
         <span className="categories-text" title="Cyberharcèlement">
           Cyberharcèlement
         </span>
-      </a>
-      <a href={linkPath} className="categories-solitude">
+      </Link>
+      <Link to={linkPath} className="categories-solitude">
         <Solitude className="inner" />
         <span className="categories-text">Solitude</span>
-      </a>
-      <a href={linkPath} className="categories-sommeil">
+      </Link>
+      <Link to={linkPath} className="categories-sommeil">
         <Sommeil className="inner" />
         <span className="categories-text">Sommeil</span>
-      </a>
-      <a href={linkPath} className="categories-RS">
+      </Link>
+      <Link to={linkPath} className="categories-RS">
         <ReseauxSociaux className="inner" />
         <span className="categories-text">Réseaux sociaux</span>
-      </a>
-      <a href={linkPath} className="categories-jeuxVideos">
+      </Link>
+      <Link to={linkPath} className="categories-jeuxVideos">
         <JeuxVideos className="inner" />
         <span className="categories-text">Jeux vidéos</span>
-      </a>
-      <a href={linkPath} className="categories-center">
+      </Link>
+      <Link to={linkPath} className="categories-center">
         <Prevention className="inner" />
         <span className="categories-text">Prévention</span>
-      </a>
+      </Link>
     </div>
   );
 };
